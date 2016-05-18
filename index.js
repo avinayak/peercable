f313c87b95d87cc488214039e3911218901a99bd
Atul Vinayak <su.atul.vi@gmail.com>
2016-05-18 22:56:54 +0530

    updated node stuff

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var app = express();
+var counter = 1;
 
 app.set('port', (process.env.PORT || 5000));
+app.use(express.static('static'));
 
 app.get('/', function(request, response) {
-  response.send('Hello peery eyed world!');
+	counter +=1
+	response.send('you are visitor no. '+counter);
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+
+	console.log('Node app is running on port', app.get('port'));
 });
